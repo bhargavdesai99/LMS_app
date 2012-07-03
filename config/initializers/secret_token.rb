28f4bf1e4310f425b7d6d2275b07ed17c3c0b186
Bhargav Desai <bhargav.vijay.desai@accenture.com>
2012-07-03 16:22:34 +0530

    Initial commit to LMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LMSApp::Application.config.secret_token = 'df1fe3d9fa65e687e23e8743de0172c552b94d411e1232c0ce821358b7fd71869ee8148bb114cfed321561ea0bada0a22e4d0cf7c6e435f160d97a2c5e8500e8'
